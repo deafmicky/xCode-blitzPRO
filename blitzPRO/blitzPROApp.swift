f66a069b4417a590a4eac5729840c44b70fde70b
Ilja Khenkine <bryonxde@gmail.com>
2024-07-09 04:22:49 +0200

    Initial Commit

---
+++ blitzPRO/blitzPROApp.swift
@@ -0,0 +1,17 @@
+//
+//  blitzPROApp.swift
+//  blitzPRO
+//
+//  Created by Ilja Khenkine on 09.07.24.
+//
+
+import SwiftUI
+
+@main
+struct blitzPROApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
